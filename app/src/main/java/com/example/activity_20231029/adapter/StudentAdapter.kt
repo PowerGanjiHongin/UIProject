064336b6eea7b193756cdc8cc03b3905668ebfbd
Your PowerGanjiHongin <dlaghddls99@gmail.com>
2023-10-29 13:39:17 +0900

    temprow와 null에 대한 주석 추가

--- app/src/main/java/com/example/activity_20231029/adapter/StudentAdapter.kt
+++ app/src/main/java/com/example/activity_20231029/adapter/StudentAdapter.kt
@@ -9,11 +9,11 @@ import android.widget.TextView
 import com.example.activity_20231029.R
 import com.example.activity_20231029.datas.StudentData
 
-class StudentAdapter(mContext:Context,
+class StudentAdapter
+    (mContext:Context,
     resId : Int,
     val mList: ArrayList<StudentData> // getview '함수'에서도 사용하고 싶다. : val 키워드를 추가한다.
-     ) :
-    ArrayAdapter<StudentData>(mContext, resId, mList) { // ArrayAdaper: 기본 생성자 지원 x
+     ) : ArrayAdapter<StudentData>(mContext, resId, mList) { // ArrayAdaper: 기본 생성자 지원 x
     // 커스텀으로 만든 xml을 => 코틀린으로 끌어와주는 객체
     val inf = LayoutInflater.from(mContext)
     // 완성한 inf를이용해서 xml을 가져와주는 함수
@@ -21,9 +21,17 @@ class StudentAdapter(mContext:Context,
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var tempRow = convertView
 
+        // 만일 tempRow가 null상태라면 (재사용할 row가 아직 안그려짐)
+
         if (tempRow == null) {
+            //xml을  inflate해서 그려준다. 그렇게 tempRow에 담아준다.
             tempRow = inf.inflate(R.layout.student_list_item, null)
         }
+
+        // if문을 빠져나온후 원래 null이라면 새로 그려서 넣고 null이 아니라면 있는 것을 사용한다.
+        // tempdp null 상태일 가능성을 완전히 제거
+
+        // 진짜로 사용할 row 변수에 temprow가 null이 아니라고 장담하며 대입
         val row = tempRow!!
 
         // xml 을 그려낸 row 객체 내부에서, 텍스트뷰 등을 찾아서 실제 데이터로 이동
@@ -40,7 +48,7 @@ class StudentAdapter(mContext:Context,
         stdName.text = stdData.name
 
         val stdAge = row.findViewById<TextView>(R.id.txtAge)
-        stdAge.text = "(${stdData.birthYear + 1}tp)"
+        stdAge.text = "${stdData.getKoreanAge(2023)}세"
 
         val stdPhone = row.findViewById<TextView>(R.id.txtPhone)
         stdPhone.text = stdData.phoneNum
--- app/src/main/java/com/example/activity_20231029/datas/StudentData.kt
+++ app/src/main/java/com/example/activity_20231029/datas/StudentData.kt
@@ -1,5 +1,6 @@
 package com.example.activity_20231029.datas
 
+import android.util.Log
 import android.view.LayoutInflater
 
 class StudentData( //StudentData의 생성자 변경 : 이름, 출생년도, 폰번을 넣어서 생성.
@@ -9,6 +10,17 @@ class StudentData( //StudentData의 생성자 변경 : 이름, 출생년도, 폰
     // 생성자 문법체험 => 폰번을 안받는 생성자도 쓰고 싶다.
     // Main : 전부 다 받는. Sub : 폰번은 안받는(보조)
     constructor( name : String, birthYear: Int) :this(name, birthYear,"폰번 모름")
+
+    // 현재 한국식 나이를 계산해서 리턴해주는 함수
+    fun getKoreanAge( year: Int ) : Int{
+        return year - this.birthYear + 1
+    }
+
+    // void 대체 연습 : 폰번을 - 제외하고 주는 함수
+
+    fun getSimpePhoneNum(){ // 리턴 타입을 명시하지 않으면 리턴 타입이 없는 것
+        Log.d("학생 데이터", this.phoneNum.replace("-", ""))
+    }
 
 
 }
